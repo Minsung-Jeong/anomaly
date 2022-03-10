44193f8032fd1833635f960df1fc5682a158980c
minsung_jeong <wjdalstjd25@gmail.com>
2022-03-10 19:12:37 +0900

    lstm vae with err + without err

--- PycharmProjects/project22/tutorial/AnomalyDetection/practice.py
+++ PycharmProjects/project22/tutorial/AnomalyDetection/practice.py
@@ -1,20 +1,9 @@
-# 변경사항
-# 'def call' 로 되어있던 부분 'def __call__' 로 변경(왜 이런 오류가 있는 거지? 오류가 아닌가? 파이썬 버전 차이인가?, 저 형식으로도 되는 건가?)
-
-# def __init__ 은 생성자
-# def __call__ 은 인스턴스 호출 후,
-
-# from azureml.core.authentication import ServicePrincipalAuthentication
-# from azureml.core import Workspace, Dataset
-
 import numpy as np
-
 np.random.seed(0)
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 
 tf.random.set_seed(0)
@@ -26,15 +15,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-
-
-# file = open("C://data_minsung/bearing/1st_test/sample.24",'r', encoding='ascii')
-Dataset = pd.DataFrame(np.random.rand(10000, 8))
-a = Dataset.__array__()
-
 dataset_name = "bearing_dataset"
 # train_ratio = 0.75
-row_mark = int(len(Dataset)*0.5)
+row_mark = 740
 batch_size = 10
 time_step = 1
 x_dim = 4
@@ -47,32 +30,26 @@ mode = 'train'
 model_dir = "./lstm_vae_model/"
 image_dir = "./lstm_vae_images/"
 
+
 def split_normalize_data(all_df):
     # row_mark = int(all_df.shape[0] * train_ratio)
     train_df = all_df[:row_mark]
     test_df = all_df[row_mark:]
 
     scaler = MinMaxScaler()
-
-    # scaler.fit(np.array(all_df)[:, 1:])
-    # train_scaled = scaler.transform(np.array(train_df)[:, 1:])
-    # test_scaled = scaler.transform(np.array(test_df)[:, 1:])
-
-    scaler.fit(np.array(all_df)[:, :])
-    train_scaled = scaler.transform(np.array(train_df)[:, :])
-    test_scaled = scaler.transform(np.array(test_df)[:, :])
-
+    scaler.fit(np.array(all_df)[:, 1:])
+    train_scaled = scaler.transform(np.array(train_df)[:, 1:])
+    test_scaled = scaler.transform(np.array(test_df)[:, 1:])
     return train_scaled, test_scaled
 
-def reshape(input):
-    input = input.__array__()
-    return np.reshape(input, (input.shape[0], time_step, input.shape[1])).astype("float32")
-    # return da.reshape(da.shape[0], time_step, da.shape[1]).astype("float32")
+
+def reshape(da):
+    da = da.__array__()
+    return da.reshape(da.shape[0], time_step, da.shape[1]).astype("float32")
 
 
 class Sampling(layers.Layer):
     def __init__(self, name='sampling_z'):
-        # 부모 클래스에 넘길 값을 마지막 괄호에 기입
         super(Sampling, self).__init__(name=name)
 
     def call(self, inputs):
@@ -82,19 +59,18 @@ class Sampling(layers.Layer):
         epsilon = K.random_normal(shape=(mu.shape[0], z_dim), mean=0.0, stddev=1.0)
         return mu + epsilon * sigma
 
+    def get_config(self):
+        config = super(Sampling, self).get_config()
+        config.update({'name': self.name})
+        return config
 
 
 class Encoder(layers.Layer):
     def __init__(self, time_step, x_dim, lstm_h_dim, z_dim, name='encoder', **kwargs):
         super(Encoder, self).__init__(name=name, **kwargs)
 
-        # stateful 하려면 batch_size 나 batch_input_shape 설정이 있어야 하는데???
         self.encoder_inputs = keras.Input(shape=(time_step, x_dim))
-        # stateful = True 이면 배치 내의 i번째 샘플의 마지막 state가, 다음 배치의 i번째 initial state로 사용
-        # This assumes a one-to-one mapping between samples in different successive batches. = 굳이 사용해야 하나???
-        # stateful 하려면 batch_input_shape 를 첫 레이어에 넣어야 하는데 그런 부분이 없어서 에러 나올 확률이 있다
-        self.encoder_lstm = layers.LSTM(lstm_h_dim, activation='softplus', name='encoder_lstm')
-        # self.encoder_lstm = layers.LSTM(lstm_h_dim, activation='softplus', name='encoder_lstm', stateful=True)
+        self.encoder_lstm = layers.LSTM(lstm_h_dim, activation='softplus', name='encoder_lstm', stateful=True)
         self.z_mean = layers.Dense(z_dim, name='z_mean')
         self.z_logvar = layers.Dense(z_dim, name='z_log_var')
         self.z_sample = Sampling()
@@ -114,6 +90,7 @@ class Encoder(layers.Layer):
             'z_sample': self.z_sample.get_config()
         })
         return config
+
 
 class Decoder(layers.Layer):
     def __init__(self, time_step, x_dim, lstm_h_dim, z_dim, name='decoder', **kwargs):
@@ -155,18 +132,9 @@ class LSTM_VAE(keras.Model):
         mu_z, logvar_z, z = self.encoder(inputs)
         mu_x, sigma_x = self.decoder(z)
 
-        var_z = tf.math.exp(logvar_z)
-        # 원래 vae loss와 크게 다르지 않음
-        print("shape of 1.var_z, 2.logvar_z, 3.mu_z", np.shape(var_z), np.shape(logvar_z), np.shape(mu_z))
-
-
-        # kl_loss = K.mean(-0.5 * K.sum(var_z - logvar_z + tf.square(1 - mu_z), axis=1), axis=0)
-        kl_loss = tf.reduce_mean(-0.5 * tf.reduce_sum(var_z - logvar_z + tf.square(1 - mu_z), axis=1), axis=0, name='kl_loss')
-        # kl_loss = tf.reduce_mean(-0.5 * tf.zeros_like(mu_z))
-        print("kl_loss=========", kl_loss)
-        # 상속관계에 있는 모듈의 add_loss
-        # self.add_loss(kl_loss)
-        self.add_loss(lambda: kl_loss)
+        var_z = K.exp(logvar_z)
+        kl_loss = K.mean(-0.5 * K.sum(var_z - logvar_z + tf.square(1 - mu_z), axis=1), axis=0)
+        self.add_loss(kl_loss)
 
         dist = tfp.distributions.Normal(loc=mu_x, scale=tf.abs(sigma_x))
         log_px = -dist.log_prob(inputs)
@@ -181,23 +149,16 @@ class LSTM_VAE(keras.Model):
         }
         return config
 
-    # gaussian 가정했을 때의 reconstruct loss
     def reconstruct_loss(self, x, mu_x, sigma_x):
         var_x = K.square(sigma_x)
         reconst_loss = -0.5 * K.sum(K.log(var_x), axis=2) + K.sum(K.square(x - mu_x) / var_x, axis=2)
-        print("recons loss shape:", np.shape(reconst_loss))
         reconst_loss = K.reshape(reconst_loss, shape=(x.shape[0], 1))
-        # return K.mean(reconst_loss, axis=0)
-        return tf.reduce_mean(reconst_loss, axis=0, name='reconstruct')
+        return K.mean(reconst_loss, axis=0)
 
     def mean_log_likelihood(self, log_px):
-        print("log_px shape", np.shape(log_px))
         log_px = K.reshape(log_px, shape=(log_px.shape[0], log_px.shape[2]))
-        # mean_log_px = K.mean(log_px, axis=1)
-        mean_log_px = tf.reduce_mean(log_px, axis=1, name='mean_log_likeli_px')
-
-        # return K.mean(mean_log_px, axis=0)
-        return tf.reduce_mean(mean_log_px, axis=0, name='meanOfmean')
+        mean_log_px = K.mean(log_px, axis=1)
+        return K.mean(mean_log_px, axis=0)
 
     def train_step(self, data):
         if isinstance(data, tuple):
@@ -206,9 +167,8 @@ class LSTM_VAE(keras.Model):
             x = data
 
         with tf.GradientTape() as tape:
-            mu_x, sigma_x, log_px = self(x)
+            mu_x, sigma_x, log_px = self(x, training=True)
             loss = self.reconstruct_loss(x, mu_x, sigma_x)
-            # add_loss 통해 추가되었던 KLD loss를 추가
             loss += sum(self.losses)
             mean_log_px = self.mean_log_likelihood(log_px)
 
@@ -255,14 +215,7 @@ def load_model():
 
 
 def main():
-    try:
-        # dataset = Dataset.get_by_name(ws, dataset_name)
-        dataset = Dataset
-        print("Dataset found: ", dataset_name)
-    except Exception:
-        print("Dataset not found: ", dataset_name)
-
-    all_df = dataset
+    all_df = pd.DataFrame(np.random.rand(10000, 8))
     train_scaled, test_scaled = split_normalize_data(all_df)
     x_dim = train_scaled.shape[1]
     print("train and test data shape after scaling: ", train_scaled.shape, test_scaled.shape)
@@ -278,9 +231,7 @@ def main():
         train_dataset = data.Dataset.from_tensor_slices(train_X)
         train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size, drop_remainder=True)
 
-        print("모델 피팅 이전")
         history = model.fit(train_dataset, epochs=epoch_num, shuffle=False).history
-        print("마지노선_--------------------")
         model.summary()
         plot_loss_moment(history)
         save_model(model)
