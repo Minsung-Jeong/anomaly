d6507547e0ecf160d9673062141a02635284e21f
minsung_jeong <wjdalstjd25@gmail.com>
2022-03-17 20:11:56 +0900

    lstm-vae good

--- PycharmProjects/project22/tutorial/AnomalyDetection/lstmVAE2.py
+++ PycharmProjects/project22/tutorial/AnomalyDetection/lstmVAE2.py
@@ -50,6 +50,9 @@ def reshape(input):
     return np.reshape(input, (input.shape[0], time_step, input.shape[1])).astype("float32")
     # return da.reshape(da.shape[0], time_step, da.shape[1]).astype("float32")
 
+loss_metric = keras.metrics.Mean(name='loss')
+likelihood_metric = keras.metrics.Mean(name='log likelihood')
+
 
 # model 형식으로 만들어보자
 class LSTM_VAE(tf.keras.Model):
@@ -57,39 +60,23 @@ class LSTM_VAE(tf.keras.Model):
         super(LSTM_VAE, self).__init__(name=name, **kwargs)
         self.encoder = tf.keras.Sequential(
             [
-                tf.keras.layers.InputLayer(input_shape=(time_step, x_dim), name='encoder_input'),
+                tf.keras.layers.Input(shape=(time_step, x_dim), name='encoder_input'),
                 tf.keras.layers.LSTM(lstm_h_dim, activation='softplus', name='encoder_lstm'),
                 tf.keras.layers.Dense(z_dim + z_dim, name='enc_representation')
-            ]
+            ], name='encoder'
         )
-        # ------------
-        # encoder = tf.keras.Sequential(
-        #     [
-        #         tf.keras.layers.InputLayer(input_shape=(time_step, x_dim), name='encoder_input'),
-        #         tf.keras.layers.LSTM(lstm_h_dim, activation='softplus', name='encoder_lstm'),
-        #         tf.keras.layers.Dense(z_dim + z_dim, name='enc_representation')
-        #     ]
-        # )
-        # def xxx(x):
-        #     tf.split(encoder(x), num_or_size_splits=2, axis=1)
-        #     xxx(all_df)
-        # xxx(train_dataset)
-
-        # abc = tf.zeros([2,1,4])
-        # a = tf.split(abc, num_or_size_splits=2, axis=-1)[0]
-        # np.shape(a)
-        # ------
-
         self.decoder = tf.keras.Sequential(
             [
                 tf.keras.layers.RepeatVector(time_step, name='repeat_vector'),
                 tf.keras.layers.LSTM(lstm_h_dim, activation='softplus', return_sequences=True,
                                                name='decoder_lstm'),
                 tf.keras.layers.Dense(x_dim + x_dim)
-            ]
+            ], name='decoder'
         )
+
     def call(self, x):
         # mean, logvar 값을 구한 뒤 reparameterize
+        print('encoding 시작')
         mu_z, logvar_z = tf.split(self.encoder(x), num_or_size_splits=2, axis=-1)
         eps = tf.random.normal(shape=mu_z.shape)
         z = eps * tf.exp(logvar_z * 0.5) + mu_z
@@ -102,7 +89,7 @@ class LSTM_VAE(tf.keras.Model):
 
         dist = tfp.distributions.Normal(loc=mu_x, scale=tf.abs(sigma_x))
         log_px = -dist.log_prob(x)
-
+        print('call 완료')
         return mu_x, sigma_x, log_px
 
     def reconstruct_loss(self, x, mu_x, sigma_x):
@@ -114,8 +101,7 @@ class LSTM_VAE(tf.keras.Model):
     def mean_log_likelihood(self, log_px):
         log_px = tf.reshape(log_px, shape=(log_px.shape[0], log_px.shape[2]))
         mean_log_px = tf.reduce_mean(log_px, axis=1, name='mean_log_likeli_px')
-        return tf.reduce_mean(mean_log_px, axis=0, name='mean of mean')
-
+        return tf.reduce_mean(mean_log_px, axis=0, name='meanofmean')
 
     def train_step(self, data):
         if isinstance(data, tuple):
@@ -127,14 +113,21 @@ class LSTM_VAE(tf.keras.Model):
             mu_x, sigma_x, log_px = self(x)
             loss = self.reconstruct_loss(x, mu_x, sigma_x)
             loss += sum(self.losses)
+            print('loss까지 왔다')
             mean_log_px = self.mean_log_likelihood(log_px)
 
         grads = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
-        return loss
+
+        loss_metric.update_state(loss)
+        likelihood_metric.update_state(mean_log_px)
+        # print('-----------done with train step, loss:', loss)
+        # return loss
+        return {'loss': loss_metric.result(), 'log_likelihood': likelihood_metric.result()}
+
 
 # tf.function 이용할 때는 해당 명령통해 eager execution 하는 편인 듯
-tf.config.run_functions_eagerly(True)
+# tf.config.run_functions_eagerly(True)
 # Dataset = pd.DataFrame(np.random.rand(10000, 8))
 
 all_df = Dataset
@@ -151,16 +144,13 @@ mode = 'train'
 model_dir = "./lstm_vae_model/"
 image_dir = "./lstm_vae_images/"
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, epsilon=1e-6, amsgrad=True)
+opt = tf.keras.optimizers.Adam(learning_rate=0.001, epsilon=1e-6, amsgrad=True)
 if mode == "train":
     model = LSTM_VAE(time_step, x_dim, lstm_h_dim, z_dim, dtype='float32')
-
-    model.compile(optimizer=optimizer)
+    model.compile(optimizer=opt)
     train_dataset = tf.data.Dataset.from_tensor_slices(train_X)
     train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size, drop_remainder=True)
-
     history = model.fit(train_dataset, epochs=epoch_num, shuffle=False).history
-
     model.summary()
 
 # elif mode == "infer":
