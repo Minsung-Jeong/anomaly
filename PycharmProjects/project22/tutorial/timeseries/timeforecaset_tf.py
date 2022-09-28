82c13cfcdfa05513df8a4c86d4c5408a5a0fdca8
minsung_jeong@tmax.co.kr <wjdalstjd25@gmail.com>
2022-09-28 16:57:53 +0900

    corp 0928

---
+++ PycharmProjects/project22/tutorial/timeseries/timeforecaset_tf.py
@@ -0,0 +1,155 @@
+import os
+import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+
+zip_path = tf.keras.utils.get_file(
+    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
+
+df = pd.read_csv(csv_path)
+np.shape(df)
+# Slice [start:stop:step], starting from index 5 take every 6th record.
+df = df[5::6]
+date_time = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
+
+# plot_cols = ['T (degC)', 'p (mbar)', 'rho (g/m**3)']
+# plot_features = df[plot_cols]
+# plot_features.index = date_time
+# _ = plot_features.plot(subplots=True)
+#
+# plot_features = df[plot_cols][:480]
+# plot_features.index = date_time[:480]
+# _ = plot_features.plot(subplots=True)
+
+"""
+inspect and clean up
+"""
+
+df.describe().transpose()
+
+wv = df['wv (m/s)']
+bad_wv = wv == -9999.0
+wv[bad_wv] = 0.0
+
+max_wv = df['max. wv (m/s)']
+bad_max_wv = max_wv == -9999.0
+max_wv[bad_max_wv] = 0.0
+
+# The above inplace edits are reflected in the DataFrame.
+df['wv (m/s)'].min()
+
+# 각도 데이터 전처리
+wv = df.pop('wv (m/s)')
+max_wv = df.pop('max. wv (m/s)')
+
+# Convert to radians.
+wd_rad = df.pop('wd (deg)')*np.pi / 180
+
+# Calculate the wind x and y components.
+df['Wx'] = wv*np.cos(wd_rad)
+df['Wy'] = wv*np.sin(wd_rad)
+
+# Calculate the max wind x and y components.
+df['max Wx'] = max_wv*np.cos(wd_rad)
+df['max Wy'] = max_wv*np.sin(wd_rad)
+
+timestamp_s = date_time.map(pd.Timestamp.timestamp)
+
+day = 24*60*60
+year = (365.2425)*day
+
+df['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+df['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+df['Year sin'] = np.sin(timestamp_s * (2 * np.pi / year))
+df['Year cos'] = np.cos(timestamp_s * (2 * np.pi / year))
+
+fft = tf.signal.rfft(df['T (degC)'])
+f_per_dataset = np.arange(0, len(fft))
+
+n_samples_h = len(df['T (degC)'])
+hours_per_year = 24*365.2524
+years_per_dataset = n_samples_h/(hours_per_year)
+
+f_per_year = f_per_dataset/years_per_dataset
+plt.step(f_per_year, np.abs(fft))
+plt.xscale('log')
+plt.ylim(0, 400000)
+plt.xlim([0.1, max(plt.xlim())])
+plt.xticks([1, 365.2524], labels=['1/Year', '1/day'])
+_ = plt.xlabel('Frequency (log scale)')
+
+column_indices = {name: i for i, name in enumerate(df.columns)}
+
+n = len(df)
+train_df = df[0:int(n*0.7)]
+val_df = df[int(n*0.7):int(n*0.9)]
+test_df = df[int(n*0.9):]
+
+num_features = df.shape[1]
+
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
+# 데이터 전처리 후 시각화
+df_std = (df - train_mean) / train_std
+df_std = df_std.melt(var_name='Column', value_name='Normalized')
+plt.figure(figsize=(12, 6))
+ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+_ = ax.set_xticklabels(df.keys(), rotation=90)
+
+"""
+Data window
+1. <single-output>, and multi-output predictions
+2. single-time-step and multi-time-step predictions
+"""
+
+class WindowGenerator():
+    def __init__(self, input_width, label_width, shift,
+                 train_df=train_df, val_df=val_df, test_df=test_df,
+                 label_columns=None):
+        # Store the raw data.
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+
+        # Work out the label column indices.
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {name: i for i, name in
+                                          enumerate(label_columns)}
+        self.column_indices = {name: i for i, name in
+                               enumerate(train_df.columns)}
+
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
