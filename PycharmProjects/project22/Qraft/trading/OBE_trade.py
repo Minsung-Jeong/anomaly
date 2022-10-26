d0d5394fd2a8fdeaef2ca44c17537dbd51a37917
minsung_jeong <wjdalstjd25@gmail.com>
2022-10-26 18:09:19 +0900

    corp 1026

--- PycharmProjects/project22/Qraft/trading/OBE_trade.py
+++ PycharmProjects/project22/Qraft/trading/OBE_trade.py
@@ -20,13 +20,21 @@ def get_mdd(x):
     peak_upper = np.argmax(arr_v[:peak_lower])
     return peak_upper, peak_lower, (arr_v[peak_lower] - arr_v[peak_upper]) / arr_v[peak_upper]
 
-input = pd.read_csv("C://data_minsung/finance/trade/JPY_KRW_2022.csv").iloc[::-1]
-input.columns = ['date', 'close', 'open', 'high', 'low', 'volume', 'volatil']
-input["date"] = pd.to_datetime(input["date"])
-input.set_index(["date"], inplace=True)
+input = pd.read_csv("C://data_minsung/finance/trade/ovv_trade.csv").iloc[::-1]
+input["Date"] = pd.to_datetime(input["Date"])
+
+input.set_index(["Date"], inplace=True)
+
+float(input["Open"].iloc[0][1:])
+
+input["Open"] = [float(x[1:]) for x in input["Open"]]
+
+
+# 1년 정도로만 제한해서 해보기
+input = input.iloc[-100:]
 
 # plot open data
-open_plot = pd.DataFrame(input["open"].values.reshape(-1,1), index=input.index)
+open_plot = pd.DataFrame(input["Open"].values.reshape(-1,1), index=input.index)
 plt.plot(open_plot, color='black')
 # temp_plot = open_plot[-60:-30]
 # plt.plot(temp_plot)
@@ -71,11 +79,11 @@ plt.plot(open_plot, color='black')
 # 21년에 대해서도 가능한지 확인 -> 12%의 수익률
 # 기준 가격과 diff에 대해서 window 계속 움직일 수 있게 해야함
 """
-open_val = input["open"].values
+open_val = input["Open"].values
 open_idx = input.index
 split = 5
-# 환율 우대 80%~95%
-comisison = 20*(1-0.80) / 100
+# 주식 - 슬리피지 고려 x
+comisison = 20*(1-1) / 100
 
 
 trade = 0
@@ -86,30 +94,33 @@ buy_pr = []
 sell_idx = []
 sell_pr = []
 account_with_date = pd.DataFrame(np.zeros((len(input), split)), index=input.index)
+mdd_li = []
 
 for i in range(30, len(open_val)):
-    temp = input["open"].iloc[i-30:i]
+    temp = input["Open"].iloc[i-30:i]
     max_idx, min_idx, mdd = get_mdd(temp)
+    mdd_li.append(mdd)
     pric_standard = np.mean(temp)
     diff_standard = abs(mdd) / split
 
     # 최초매수 : 계좌에 외화가 없을 때
-    if open_val[i] < pric_standard and len(account) == 0 and open_val[i] < 9.9:
-    # if open_val[i] < pric_standard and len(account) == 0:
+    # if open_val[i] < pric_standard and len(account) == 0 and open_val[i] < 9.9:
+    if open_val[i] < pric_standard and len(account) == 0:
         account.append(open_val[i])
         buy_idx.append(open_idx[i])
         buy_pr.append(open_val[i])
         trade += 1
         print("start buy")
     # 매수로직 : 가격 더 떨어지면 구매, 5개 미만일 때 구매
-    if 0< len(account) < split and account[-1]-diff_standard > open_val[i] and open_val[i] < 9.9:
-    # if 0< len(account) < split and account[-1]-diff_standard > open_val[i]:
+    # if 0< len(account) < split and account[-1]-diff_standard > open_val[i] and open_val[i] < 9.9:
+    if 0< len(account) < split and account[-1]-diff_standard > open_val[i]:
         account.append(open_val[i])
         buy_idx.append(open_idx[i])
         buy_pr.append(open_val[i])
         trade += 1
     # 매도로직 : 가장 최근 매수가격 보다 오르면 매도(수수료 계산)
     if 0 < len(account) and open_val[i] > account[-1]+diff_standard+comisison:
+        # 수익률 =  현재가 / 구매가 - 1
         rev = (open_val[i]-comisison)/account.pop()-1
         result.append(rev)
         sell_idx.append(open_idx[i])
@@ -120,7 +131,7 @@ for i in range(30, len(open_val)):
 a = pd.DataFrame(buy_pr, index=buy_idx)
 b = pd.DataFrame(sell_pr, index=sell_idx)
 
-print(np.sum(result), trade)
+
 
 plt.plot(open_plot, color='black')
 plt.scatter(a.index, a.values, color='red')
@@ -130,6 +141,10 @@ plt.scatter(b.index, b.values, color='blue')
 recent = open_val[-1]
 account_rev = sum([(recent/x-1) for x in account])/ split
 
-table = pd.concat((a,b, account_with_date), axis=1)
+print(np.sum(result)+account_rev, trade)
 
-# table.to_csv("C://data_minsung/finance/trade/result/result2021.csv")
+
+table = pd.concat((a,b, account_with_date), axis=1)
+# table.to_csv("C://data_minsung/finance/trade/result/obe_2022.csv")
+
+table.to_csv("C://data_minsung/finance/trade/result/ovv_2022.csv")
