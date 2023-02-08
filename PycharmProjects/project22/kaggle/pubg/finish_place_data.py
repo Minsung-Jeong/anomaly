2e0369725ee490888f5173173f7b8966d3ef2da3
minsung_Jeong <wjdalstjd25@gmail.com>
2023-02-08 23:41:53 +0900

    home0208

--- PycharmProjects/project22/kaggle/pubg/finish_place_data.py
+++ PycharmProjects/project22/kaggle/pubg/finish_place_data.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
+import math
 
 train = pd.read_csv("C://data_minsung/kaggle/pubg/train_V2.csv")
 
@@ -16,14 +15,15 @@ len(train.columns)
 # 해당 게임은 실력이 비슷한 사람끼리 매칭됨
 # killpoints, rankpoints, winpoints 와 winPlacePerc는 상관성 낮음 -> 매칭시스템이 잘 잡혀 있다는 의미로 해석
 # 그것말고는 당장 의미를 찾을 부분은 없을 듯
-sns.jointplot(x="rankPoints", y="winPlacePerc",  data=train, height=10, ratio=3, color="lime")
+sns.jointplot(x="rankPoints", y="winPlacePerc", data=train, height=10, ratio=3, color="lime")
 train[['rankPoints', 'winPlacePerc']].corr()
 
-sns.jointplot(x="winPoints", y="winPlacePerc",  data=train, height=10, ratio=3, color="lime")
+sns.jointplot(x="winPoints", y="winPlacePerc", data=train, height=10, ratio=3, color="lime")
 train[['winPoints', 'winPlacePerc']].corr()
 
-sns.jointplot(x="killPoints", y="winPlacePerc",  data=train, height=10, ratio=3, color="lime")
+sns.jointplot(x="killPoints", y="winPlacePerc", data=train, height=10, ratio=3, color="lime")
 train[['killPoints', 'winPlacePerc']].corr()
+
 
 # simple shot
 def simple_shot(var):
@@ -32,15 +32,16 @@ def simple_shot(var):
     print('top 1% of {} : {}'.format(var, train[var].quantile(0.99)))
     print('max of {} : {}'.format(var, train[var].max()))
 
+
 # Killer에 대한 정보(평균적으로 1킬을 못함, top 1%는 7킬, 최대킬은 72킬)
 simple_shot('kills')
 
 # kill를 구분화해서 countplot
 data = train.copy()
 data.loc[data['kills'] > data['kills'].quantile(0.99)] = '8+'
-plt.figure(figsize=(15,10))
+plt.figure(figsize=(15, 10))
 sns.countplot(data['kills'].astype('str').sort_values())
-plt.title("Kill Count",fontsize=15)
+plt.title("Kill Count", fontsize=15)
 plt.show()
 
 # 같은 랭킹 내에서 킬당 'winPlacePerc'
@@ -48,6 +49,26 @@ plt.show()
 simple_shot('rankPoints')
 plt.hist(train['rankPoints'])
 plt.scatter(round(train['rankPoints'].median()), 1500000, c='red')
+plt.show()
 
-# sns.countplot(train['rankPoints'].astype('str').sort_values())
-# train[['rankPoints','winPlacePerc']]
+criteria = math.trunc((train['rankPoints'].max() - train['rankPoints'].min()) / 5)
+
+# split rankPoints into 5 levels
+# temp = []
+# for i in range(len(train)):
+#     if train['rankPoints'][i] < criteria:
+#         temp.append(1)
+#     elif criteria <= train['rankPoints'][i] < criteria * 2:
+#         temp.append(2)
+#     elif criteria * 2 <= train['rankPoints'][i] < criteria * 3:
+#         temp.append(3)
+#     elif criteria * 3 <= train['rankPoints'][i] < criteria * 4:
+#         temp.append(4)
+#     elif criteria * 4 <= train['rankPoints'][i]:
+#         temp.append(5)
+
+train['rankLevel'] = temp
+
+rank_kill = train[['winPlacePerc', 'rankLevel', 'kills']].groupby(['rankLevel', 'kills']).mean()
+
+train.to_csv("C://data_minsung/kaggle/pubg/train2.csv")
