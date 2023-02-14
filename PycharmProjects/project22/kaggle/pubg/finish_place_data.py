4605fd6148cc663515917170207a54c799afdf53
minsung_jeong <wjdalstjd25@gmail.com>
2023-02-14 19:54:06 +0900

    corp0214

--- PycharmProjects/project22/kaggle/pubg/finish_place_data.py
+++ PycharmProjects/project22/kaggle/pubg/finish_place_data.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
-
+import time
 train = pd.read_csv("C://data_minsung/kaggle/pubg/train2.csv")
 
 train.info()
@@ -18,103 +18,123 @@ plt.show()
 # walkDistance, boosts, weaponsAcquired, kills
 corr_rank = train.corr()["winPlacePerc"].sort_values(ascending=False)
 
-# 해당 게임은 실력이 비슷한 사람끼리 매칭됨
-# killpoints, rankpoints, winpoints 와 winPlacePerc는 상관성 낮음 -> 매칭시스템이 잘 잡혀 있다는 의미로 해석
-# 그것말고는 당장 의미를 찾을 부분은 없을 듯
-# sns.jointplot(x="rankPoints", y="winPlacePerc", data=train, height=10, ratio=3, color="lime")
-# train[['rankPoints', 'winPlacePerc']].corr()
-#
-# sns.jointplot(x="winPoints", y="winPlacePerc", data=train, height=10, ratio=3, color="lime")
-# train[['winPoints', 'winPlacePerc']].corr()
-#
-# sns.jointplot(x="killPoints", y="winPlacePerc", data=train, height=10, ratio=3, color="lime")
-# train[['killPoints', 'winPlacePerc']].corr()
-
-
 # simple shot
 def simple_shot(var):
     print('mean value of {} : {}'.format(var, train[var].mean()))
     print('median value of {} : {}'.format(var, train[var].median()))
     print('top 1% of {} : {}'.format(var, train[var].quantile(0.99)))
     print('max of {} : {}'.format(var, train[var].max()))
+    print('min of {} : {}'.format(var, train[var].min()))
 
 # -------------------------------------kill
 # Killer에 대한 정보(평균적으로 1킬을 못함, top 1%는 7킬, 최대킬은 72킬)
 simple_shot('kills')
 
-# kill를 구분화해서 countplot
-# data = train.copy()
-# data.loc[data['kills'] > data['kills'].quantile(0.99)] = '8+'
-# plt.figure(figsize=(15, 10))
-# sns.countplot(data['kills'].astype('str').sort_values())
-# plt.title("Kill Count", fontsize=15)
-# plt.show()
+
 
 # 킬과 승리의 상관관계(0.4199로 높지 않음)
 sns.jointplot(x="winPlacePerc", y="kills", data=train, height=10, ratio=3, color="r")
 plt.show()
 kill_win_corr = train[["winPlacePerc", "kills"]].corr()
 
-# 같은 랭킹 내에서 킬과 'winPlacePerc'
-# 랭킹을 5단계로 구분
+# 킬과 승리에 대한 scatter plot
 simple_shot('rankPoints')
 plt.hist(train['rankPoints'])
 plt.scatter(round(train['rankPoints'].median()), 1500000, c='red')
 plt.show()
 
-# split rankPoints into 5 levels
-# -1에서 5910까지를 나누어 다섯 개의 랭크 생성
-def rank_split(var_name):
-    criteria = math.trunc((train[var_name].max() - train[var_name].min()) / 5)
+def rank_split(var):
     temp = []
+    crit1 = var.quantile(0.2)
+    crit2 = var.quantile(0.4)
+    crit3 = var.quantile(0.6)
+    crit4 = var.quantile(0.8)
     for i in range(len(train)):
-        if train[var_name][i] < criteria:
+        if var[i] <= crit1:
             temp.append(1)
-        elif criteria <= train[var_name][i] < criteria * 2:
+        elif crit1 < var[i] <= crit2:
             temp.append(2)
-        elif criteria * 2 <= train[var_name][i] < criteria * 3:
+        elif crit2 < var[i] <= crit3:
             temp.append(3)
-        elif criteria * 3 <= train[var_name][i] < criteria * 4:
+        elif crit3 < var[i] <= crit4:
             temp.append(4)
-        elif criteria * 4 <= train[var_name][i]:
+        elif crit4 < var[i]:
             temp.append(5)
     return temp
 
-train['rankPoints_split'] = rank_split('rankPoints')
-# rank_kill = train[['winPlacePerc', 'rankLevel', 'kills']].groupby(['rankLevel', 'kills']).mean()
-# train.to_csv("C://data_minsung/kaggle/pubg/train2.csv")
+def split_corr(input_val):
+    temp = input_val.stack()
+    temp.index = input_val.index
+
+    li = []
+    for i in range(1, 6):
+        val = temp[i].values
+        idx = temp[i].index
+        correl = pd.DataFrame([idx, val]).T.corr()
+        res = correl[0][1]
+        li.append(res)
+        print("rank {}'s correlation :{}".format(i, res))
+    return li
 
 # 랭크 내에서 킬에 따른 치킨확률 시각화
+train['rankPoints_split'] = rank_split(train['rankPoints'])
 rank_kill = train[['winPlacePerc', 'rankPoints_split', 'kills']].groupby(['rankPoints_split', 'kills']).mean()
+
 ax = rank_kill.unstack(level=0).plot(kind='bar', subplots=True, rot=0, figsize=(9, 7), layout=(2, 3))
 plt.tight_layout()
 
 
-temp = rank_kill.stack()
-temp.index = rank_kill.index
+# 결론 : 높은 랭크에서 킬과 승리의 상관관계가 높다. 낮은 랭크는 킬이 실력을 뜻하지 않는다
+rank_kill_corr = split_corr(rank_kill)
 
-# 랭크 포인트가 높을 수록 킬과 승리의 상관관계는 높다
-for i in range(1, 6):
-    val = temp[i].values
-    idx = temp[i].index
-    correl = pd.DataFrame([idx, val]).T.corr()
-    res = correl[0][1]
-    print("rank {}'s correlation :{}".format(i, res))
+# 팀킬에 대해 알아보자
+simple_shot('teamKills')
+# 팀킬러는 거의 없다(97.8퍼센트가 팀킬0)
+sns.displot(train['teamKills'])
+plt.show()
 
+# 팀킬 0, 보다 팀킬 한 사람 승률높음(innocent, troller의 데이터 불균형 심함 or bad guys are prone to survive)
+innocent = train[train['teamKills'] == 0]['winPlacePerc'].mean()
+troller = train[train['teamKills'] !=0]['winPlacePerc'].mean()
 
 
 # ----------------------------walkDistance
 # 존버 승리자의 비율 - kill 낮으면서, walkDistance 높은 사람의 승리확률
-# 많이 움직일 수록 싸우지 않고 이길 수 있다
+# 걷는거리 랭크 내에서 킬과 승리의 상관관계
 # 4,5번 split 보면 분석결과 마라토너는 킬을 적게 한다. 최상위 마라토너들은 솔로가 아니라 듀오 또는 스쿼드
 
+# 심플샷
 simple_shot('walkDistance')
 
-train['walk_split'] = rank_split('walkDistance')
+# 데이터분포
+data = train.copy()
+data = data[data['walkDistance'] < train['walkDistance'].quantile(0.99)]
+sns.displot(data['walkDistance'])
+plt.show()
+
+# x : 승리, y : 걸은 거리
+sns.jointplot(x="winPlacePerc", y="walkDistance",  data=train, height=10, ratio=3, color="lime")
+plt.show()
+
+# 걷는 거리에 대한 rank_split
+train['walk_split'] = rank_split(train['walkDistance'])
+
+# split 이후 킬과 승리의 상관성
 walk_kill_win = train[['winPlacePerc', 'walk_split', 'kills']].groupby(['walk_split', 'kills']).mean()
 ax = walk_kill_win.unstack(level=0).plot(kind='bar', subplots=True, rot=0, figsize=(9, 7), layout=(2, 3))
-plt.tight_layout()
+plt.tight_layout() #plot보다 아래에서 상관계수 뽑는게 더 직관적
+walk_kill_win_corr = split_corr(walk_kill_win) #많이 걷는 그룹에서 킬과 승리의 상관성이 낮게 나옴
 
-# rank_walk_win = train[['winPlacePerc', 'rankPoints_split', 'walk_split']].groupby(['rankPoints_split', 'walk_split']).mean()
-# temp =train[['rankPoints_split', 'walk_split']].groupby(['rankPoints_split']).mean()
+## 장거리 마라토너로 승리하는 사람은 솔로가 아닐 확률이 높다? - 장거리 마라토너는 무임승차자가 많다
+solos = train[train['numGroups']>50]
+# duos = train[(train['numGroups']>25) & (train['numGroups']<=50)]
+# squads = train[train['numGroups']<=25]
+teamPlayers = train[train['numGroups'] <=50]
 
+# 'numGroups'으로 나눈뒤 walkDistance와 win의 상관성
+sol_walk_kill_win = solos[['walk_split', 'winPlacePerc', 'kills']].groupby(['walk_split', 'kills']).mean()
+team_walk_kill_win = teamPlayers[['walk_split', 'winPlacePerc', 'kills']].groupby(['walk_split', 'kills']).mean()
+
+
+sol_walk_kill_win_corr = split_corr(sol_walk_kill_win) #솔로 : 많이 걷는 팀이 킬과 승리의 상관성이 높음
+team_walk_kill_win_corr = split_corr(team_walk_kill_win) # 팀 : 많이 걷는 팀에서 킬과 승리의 상관성이 낮음
