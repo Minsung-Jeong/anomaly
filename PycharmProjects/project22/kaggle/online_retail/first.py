69492322a4903df98a27a3f272b16941e2dc0dfa
minsung_jeong <wjdalstjd25@gmail.com>
2023-03-07 17:46:15 +0900

    0307

--- PycharmProjects/project22/kaggle/online_retail/first.py
+++ PycharmProjects/project22/kaggle/online_retail/first.py
@@ -12,3 +12,48 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
 df = pd.read_excel("C://data_minsung/kaggle/online_retail/Online Retail.xlsx")
+
+df = df.dropna(subset=["CustomerID"])
+
+df.isnull().sum()
+df.describe()
+
+df['Quantity'].head()
+len(df)
+df = df[(df['Quantity']>0) & (df['UnitPrice']>0)]
+len(df)
+
+# cohort analysis
+def get_month(x):
+    return dt.datetime(x.year ,x.month,1)
+
+# invoiceMonth 그냥 구매일, cohorMonth 최초 구매일
+df['InvoiceMonth'] = df['InvoiceDate'].apply(get_month)
+grouping = df.groupby('CustomerID')['InvoiceMonth']
+df['CohortMonth'] = grouping.transform('min')
+df.tail()
+
+
+def get_month_int (dframe,column):
+    year = dframe[column].dt.year
+    month = dframe[column].dt.month
+    day = dframe[column].dt.day
+    return year, month , day
+
+invoice_year,invoice_month,_ = get_month_int(df,'InvoiceMonth')
+cohort_year,cohort_month,_ = get_month_int(df,'CohortMonth')
+
+year_diff = invoice_year - cohort_year
+month_diff = invoice_month - cohort_month
+
+# cohortIndex = (구매일 - 첫구매) + 1
+df['CohortIndex'] = year_diff * 12 + month_diff + 1
+
+
+#Count monthly active customers from each cohort
+grouping = df.groupby(['CohortMonth', 'CohortIndex'])
+cohort_data = grouping['CustomerID'].apply(pd.Series.nunique)
+# Return number of unique elements in the object.
+cohort_data = cohort_data.reset_index()
+cohort_counts = cohort_data.pivot(index='CohortMonth',columns='CohortIndex',values='CustomerID')
+cohort_counts
