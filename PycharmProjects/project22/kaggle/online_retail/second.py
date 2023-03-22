8d29f051cb23875a94e8ab3d4ebb4f729e25c9f2
minsung_jeong <wjdalstjd25@gmail.com>
2023-03-22 17:54:05 +0900

    corp0322

--- PycharmProjects/project22/kaggle/online_retail/second.py
+++ PycharmProjects/project22/kaggle/online_retail/second.py
@@ -7,7 +7,7 @@ import seaborn as sns
 from lifetimes import BetaGeoFitter
 from lifetimes import GammaGammaFitter
 from lifetimes.plotting import plot_period_transactions
-
+from sklearn.preprocessing import MinMaxScaler
 
 #For Machine Learning Algorithm
 from sklearn.preprocessing import StandardScaler
@@ -89,7 +89,6 @@ print('Min Invoice Date:',df.InvoiceDate.dt.date.min(),'max Invoice Date:',
 # last day + 1
 pin_date = df['InvoiceDate'].max() + dt.timedelta(days=1)
 
-
 rfm = df.groupby(['CustomerID']).agg({'InvoiceDate':lambda x : (pin_date-x.max()).days,
                                       'InvoiceNo':'count',
                                       'TotalSum':'sum'})
@@ -120,21 +119,55 @@ difference between churn rates follows Beta
 purchase follows poisson 
 difference between purchases follows Gamma 
 """
-rfm
-bgf = BetaGeoFitter(penalizer_coef=0.001) #BG/NBD 모델 중 BG모델 적용 = 고객 이탈에 대한 추정
-bgf.fit(rfm['Frequency'],rfm['Recency'],rfm['T'])
+# 개별 이탈 고객은 기하분포, 고객간 이탈 이질성은 베타분포 -> BG 통해서 고객이탈 추정
+# nbd : 포아송(개별 고객 구매빈도), 감마(고객간 재구매 차이)를 통해서 ->  생존기간 동안의 기대구매횟수 도출
+bgf = BetaGeoFitter(penalizer_coef=0.001)
+bgf.fit(rfm['Frequency'],rfm['Recency'],rfm['T']) # fit = fit dataset to a BG/NBD model
 
-# 1 = 1week, top10 customers who are expected to purchase much in a week.
+# T동안의 기대구매 횟수 도출
 bgf.conditional_expected_number_of_purchases_up_to_time(1,rfm['Frequency'],rfm['Recency'],rfm['T'])\
     .sort_values(ascending=False).head(10)
 
+# 위랑 같은 함수 predict = conditional_expected_number_of_purchases_up_to_time
 rfm["buy_1_week_pred"] = bgf.predict(1,rfm['Frequency'],rfm['Recency'],rfm['T'])
 rfm["buy_1_month_pred"] = bgf.predict(4,rfm['Frequency'],rfm['Recency'],rfm['T'])
 
 # sort해서 보기
 rfm.sort_values("buy_1_month_pred", ascending=False).head()
 
-# 한 달간 예상되는 총매출
-rfm["buy_1_month_pred"].sum()
 
-# trn/test 나눠서 해보기
+# rfm["buy_1_month_pred"].sum() # 한 달간 예상되는 총매출
+# plot_period_transactions(bgf)
+# plt.show()
+
+# gamma-gamma model : expected average profit with gamma-gamma model
+rfm.columns
+ggf = GammaGammaFitter(penalizer_coef=0.01)
+ggf.fit(rfm["Frequency"], rfm["Monetary"])
+# the conditional expectation of the average profit per transaction for a group of one or more customers
+ggf.conditional_expected_average_profit(rfm["Frequency"], rfm["Monetary"])
+
+rfm["expected_average_profit"] = ggf.conditional_expected_average_profit(rfm["Frequency"], rfm["Monetary"])
+
+# T동안 고객의 기대구매횟수 * 고객당 평균 예상 수익(구매액) * 기간
+cltv = ggf.customer_lifetime_value(bgf, rfm['Frequency'], rfm['Recency'], rfm['T'], rfm['Monetary'],
+ time=3, # 3 Months
+ freq='W', # Frequency of T ,in this case it is 'weekly'
+ discount_rate=0.01)
+cltv = cltv.reset_index()
+cltv_final = rfm.merge(cltv, on="CustomerID", how="left")
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaler.fit(cltv_final[["clv"]])
+
+cltv_final["scaled_clv"] = scaler.transform(cltv_final[["clv"]])
+cltv_final.sort_values(by="scaled_clv", ascending=False).head()
+cltv_final["clv_segment"] = pd.qcut(cltv_final["scaled_clv"], 4, labels=["D", "C", "B", "A"])
+cltv_final.head()
+cltv_final.sort_values(by="scaled_clv", ascending=False).head(10)
+
+
+
+cltv_final.sort_values(by="RFM_Score", ascending=False).iloc[:10][['CustomerID', 'scaled_clv']]
+cltv_final.sort_values(by="RFM_Score", ascending=False).iloc[-10:][['CustomerID', 'scaled_clv']]
+
